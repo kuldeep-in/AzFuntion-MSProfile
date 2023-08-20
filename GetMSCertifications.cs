e34c3c9c20dc9d75ba78d7abd5a52f60db4d7800
kuldeepsingh-in <in.singhkuldeep@gmail.com>
2023-08-20 13:44:14 +0100

    Updated code to Generate Auth Token

--- GetMSCertifications.cs
+++ GetMSCertifications.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.Identity.Client;
+using Azure.Identity;
+using Azure.Core;
 
 namespace fn_msProfile
 {
@@ -20,11 +22,42 @@ namespace fn_msProfile
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-
             string clientId = Environment.GetEnvironmentVariable("ClientId");
+            string tenantId = Environment.GetEnvironmentVariable("TenantId");
+            string clientSecret = Environment.GetEnvironmentVariable("ClientSecret");
+            string authority = $"https://login.microsoftonline.com/{tenantId}";
+            string authToken = string.Empty;
+
+            string[] scopes = new string[] { $"https://graph.microsoft.com/.default" };
+            //string[] scopes = new string[] { $"{clientId}/.default" };
+            //string[] scopes = new string[] { $"{clientId}/.default" };
+            //string[] scopes = { "user.read" };
+
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+                .Create(clientId)
+                .WithTenantId(tenantId)
+                //.WithAuthority(authority)
+                .WithClientSecret(clientSecret)
+                .Build();
 
 
+            // Option 01 - using Authorize Code
+            var authRequestUrl = confidentialClientApplication.GetAuthorizationRequestUrl(scopes);
+            string authorizationCode = "authorizationCode";
+            AuthenticationResult authResult = await confidentialClientApplication.AcquireTokenByAuthorizationCode(scopes, authorizationCode).ExecuteAsync();
+            authToken = authResult.AccessToken;
 
+            // Option 02 - Using TokenForClinet
+            AuthenticationResult authResult1 = await confidentialClientApplication.AcquireTokenForClient(scopes).ExecuteAsync();
+            authToken = authResult1.AccessToken;
+
+            // Option 03 - Using ClientSecretCredential
+            var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+            var tokenRequestContext = new TokenRequestContext(scopes);
+            authToken = clientSecretCredential.GetTokenAsync(tokenRequestContext).Result.Token;
+
+            // Read token from any of the above 3 options
+            Console.WriteLine("Access token: {0}", authToken);
 
             string name = req.Query["name"];
 
@@ -32,9 +65,9 @@ namespace fn_msProfile
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
+            string responseMessage = string.IsNullOrEmpty(authToken)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Token:{authToken}";
 
             return new OkObjectResult(responseMessage);
         }
